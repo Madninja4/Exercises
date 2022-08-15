404d7b97ccf7e8748bac795dea7b6c23da0f6886
Madninja4 <corbincreedon@yahoo.com>
2022-08-15 01:40:01 -0400

    did todo

--- RepeatedCharacters.js
+++ RepeatedCharacters.js
@@ -1,7 +1,7 @@
 function showMostRepeatedChar(str) {
     var max = 0, // variable to store the number of appearances of the max char so far
         maxChar = ""; //string to store to the max char(s) in case of multiple characters
-    str.split("").forEach(function (char) {  //splits up the argument's string by letters and performs local function to do with them individually as a argument
+    str.split(" ").join("").split("").forEach(function (char) {  //splits up the argument's string by letters and performs local function to do with them individually as a argument
         if (str.split(char).length > max) { //checks to see how many items are in the array after splitting it by the letter and comparing it to current Max
             max = str.split(char).length; // hightens Max
             maxChar = char; //Changes maxChar
@@ -9,7 +9,6 @@ function showMostRepeatedChar(str) {
     });
     return maxChar;
 }
-
-string = "BB  BB dgdes w";
+string = "BB  BB  d    gdes w  ";
 console.log(showMostRepeatedChar(string));
-//TO DO: Make it so this does not recognize empty spaces as Chars
+//TO DO: Make it so this does not recognize empty spaces as Chars //DONE
